4cb037314e024da95a8f65b699a24776934cbd50
Kammy <blahblahkams@yahoo.com>
2015-12-25 23:58:01 -0500

    revise tests to fit current layout/features  - fix the page titles  - hid user links

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   get 'users/new'
   get 'signup' => 'users#new'
+  get 'login'  => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
 
   def setup
-    @base_title = "Vitrium"
+    @base_title = "Guru Dev"
   end
 
   test "should get home" do
@@ -13,12 +13,12 @@ class StaticPagesControllerTest < ActionController::TestCase
   end
 
   test "should get portfolio" do
-    get :about
+    get :portfolio
     assert_response :success
-    assert_select "title", "About | #{@base_title}" 
+    assert_select "title", "Portfolio | #{@base_title}" 
   end
 
-  test "should get bio" do
+  test "should get team" do
   	get :team
   	assert_response :success
   	assert_select "title", "Team | #{@base_title}" 
