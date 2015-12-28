3b14173f40befe1b0a437006fdf25e772ad3d01b
Kammy <blahblahkams@yahoo.com>
2015-12-28 17:42:27 -0500

    Add controllers for applications, proposals, and messages - update routing - add proposal page to layout

---
+++ app/controllers/team_applications_controller.rb
@@ -0,0 +1,13 @@
+class TeamApplicationsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,27 @@
 Rails.application.routes.draw do
 
+ # these will be log-in only
+  get 'messages/index'
+  get 'messages/show'
+
+  get 'proposals/index'
+  get 'proposals/show'
+
+  get 'team_applications/index'
+  get 'team_applications/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'static_pages#home'
   get 'team' 		=> 'static_pages#team'
   get 'portfolio' 	=> 'static_pages#portfolio'
-  get 'contact' 	=> 'static_pages#contact'
-  get 'join' 		=> 'static_pages#join'
+  get 'join' 		=> 'team_applications#new'
+  post 'join' 		=> 'team_applications#create'
+  get 'proposal' 	=> 'proposals#new'
+  post 'proposal' 	=> 'proposals#create'
+  get 'contact' 	=> 'messages#new'
+  post 'contact' 	=> 'messages#create'
 
   get 'users/new'
   get 'signup' => 'users#new'
