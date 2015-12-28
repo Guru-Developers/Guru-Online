a937b1663b6d7537b38832f92b60e806222d6c89
Kammy <blahblahkams@yahoo.com>
2015-12-28 17:07:15 -0500

    add basic presence validations to models for not-null fields

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,2 +1,7 @@
 class Proposal < ActiveRecord::Base
+  validates :first_name,  presence: true
+  validates :last_name,  presence: true
+  validates :email,  presence: true
+  validates :message,  presence: true
+
 end
--- app/models/team_application.rb
+++ app/models/team_application.rb
@@ -1,2 +1,8 @@
 class TeamApplication < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :date_of_birth, presence: true
+  validates :resume, presence: true
+
 end
