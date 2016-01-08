9e6ad8d66265a561a5648cfc4b4d43e54e099cd0
lgill <lgill@lgill-pc.local>
2016-01-08 04:29:46 -0500

    first draft of html content

--- db/schema.rb
+++ db/schema.rb
@@ -20,17 +20,6 @@ ActiveRecord::Schema.define(version: 20151228215457) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "patients", force: :cascade do |t|
-    t.string   "name"
-    t.string   "surgeon"
-    t.string   "email"
-    t.string   "device"
-    t.string   "annotation"
-    t.string   "hospital"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "proposals", force: :cascade do |t|
     t.string   "first_name",   null: false
     t.string   "last_name",    null: false
